8958d74bc0f81fdaf3c980e9acb85e94eb0914f1
Alvaro Bonilla LLedo <alvaro.bonilla.lledo@gmail.com>
2024-10-07 08:57:22 +0200

    web estatica

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -1,1 +1,11 @@
 console.log("Hola mundo");
+
+document.querySelector("input#lenguaje").value = navigator.language;
+
+document.querySelector("a#open-offcanvas").addEventListener('click', ()=>{
+    document.querySelector("aside#offcanvas").classList.add("show");
+});
+
+document.querySelector("button.close").addEventListener('click', ()=>{
+    document.querySelector("aside#offcanvas").classList.remove("show");
+});
