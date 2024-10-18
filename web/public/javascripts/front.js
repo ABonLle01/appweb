725c554355bfceaa217a1117d4f372e8af16e884
Alvaro Bonilla LLedo <alvaro.bonilla.lledo@gmail.com>
2024-10-18 10:18:39 +0200

    varias paginas

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -9,3 +9,12 @@ document.querySelector("a#open-offcanvas").addEventListener('click', ()=>{
 document.querySelector("button.close").addEventListener('click', ()=>{
     document.querySelector("aside#offcanvas").classList.remove("show");
 });
+
+document.querySelectorAll("#galeria .inner figure").forEach( (e)=>{
+    e.addEventListener("click", (event)=>{
+        const fig = e;
+        const image = fig.querySelector("img").src;
+        document.querySelector("dialog#lightbox img").src = image;
+        document.querySelector("dialog#lightbox").showModal();
+    })
+} )
